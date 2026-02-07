5024b34e031b00356ba6be5d8cefd38e781cf56c
satriobismaa <bismazharfan@gmail.com>
2026-02-07 13:44:51 +0700

    Add update product repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,38 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product){
+        product.setProductId(UUID.randomUUID());
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findProductById(UUID targetId){
+        for(Product product : productData){
+            if(product.getProductId().equals(targetId)){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void updateProduct(Product newProduct){
+        for(Product product : productData){
+            if(product.getProductId().equals(newProduct.getProductId())){
+                product.setProductName(newProduct.getProductName());
+                product.setProductQuantity(newProduct.getProductQuantity());
+                break;
+            }
+        }
     }
 }
