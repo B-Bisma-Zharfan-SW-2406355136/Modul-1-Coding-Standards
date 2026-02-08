be5a9a892989546ed4be777c7cbd68f868ca245b
satriobismaa <bismazharfan@gmail.com>
2026-02-08 11:37:41 +0700

    Revert "Add unit test for product model and repository"
    This reverts commit 1f0aabf52d04c33483299b6ab0f65b126507c47b.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.10"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "id.ac.ui.cs.advprog"
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.4"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
+group = "id.cs.ui.ac.advprog"
 version = "0.0.1-SNAPSHOT"
 description = "eshop"
 
@@ -29,16 +30,20 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
-	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
 	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
 tasks.register<Test>("unitTest") {
@@ -59,6 +64,6 @@ tasks.register<Test>("functionalTest") {
 	}
 }
 
-tasks.withType<Test>().configureEach {
+tasks.withType<Test>().configureEach	 {
 	useJUnitPlatform()
 }
